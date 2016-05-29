ef38acf7c1cb2ed9f4b24fc5cd1515a7ad1bad39
Arkadiusz Macak <arkadiusz.macak@atos.net>
2016-05-29 10:46:20 +0200

    it finall starts working as designed - filters

--- src/EsmAPI.py
+++ src/EsmAPI.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
     sys.exit(1)
 import xml.parsers.expat
 import base64
-import sys,time
+import sys,time,string
 import httplib,urllib,ssl
 import json
+import getpass
 from collections import deque
 
 class xp ():
@@ -31,10 +32,11 @@ class xp ():
 class API():
     conn = None
     sessionID = None
+    host = '0.0.0.0'
     result_table = deque()
     def __init__(self,host,port = 443):
         #context = ssl._create_unverified_context()#ssl.create_default_context()
-        
+        self.host = host
         #self.conn = httplib.HTTPSConnection(host,port,None,None,None,None,None,context)
         self.conn = httplib.HTTPSConnection(host,port,timeout=30)
        
@@ -47,12 +49,18 @@ class API():
             fileName = sys.path[0]+'/.sessID'
         try:
             fp = open(fileName,'r')
-            self.setSessionID(fp.read())
+            content = fp.read()
+            sessID, hostIP = string.split(content,'|')
+            if hostIP != self.host :
+                raise Exception('Lack of session ID, please login first')
+            self.setSessionID(sessID)
             fp.close()
             return True
         except:
             login = str(raw_input('User login:'))
             password = str(raw_input('Password:'))
+            
+            
             self.auth = base64.b64encode('%s:%s' % (login,password))
             if not self.conn :
                 raise Exception('Lack of established connection');
@@ -73,7 +81,7 @@ class API():
             p.Parse(xres,1)
             self.sessionID = myParser.get('sessionID')
             fp = open(fileName,'w')
-            fp.write(self.sessionID)
+            fp.write(self.sessionID+'|'+self.host)
             fp.close()
         #return self.sessionID
         return True
@@ -90,7 +98,19 @@ class API():
     def setSessionID(self,sessID):
        
         self.sessionID = sessID
-      
+    def caseGetCase(self,caseID):
+        params = '{"id" : {"value" : %s}}' % (caseID)
+        try:
+            a = self.q('caseGetCaseDetail',params)
+        except Exception as er:	
+            return (er[1],er[0])
+        res = json.loads(a)
+        return (0,res['return'])
+    def caseGetCaseList(self):
+        a = self.q('caseGetCaseList')
+        print a
+        res = json.loads(a)
+        return (0,res['return'])
     def getVersion(self):      
         res = json.loads(self.q('getVersion'))
         return res['return']
@@ -102,11 +122,11 @@ class API():
         params = '{"resultID" : {"value" : %s}}' % (resultID)
         res = json.loads(self.q('qryGetStatus',params))
         return res['return']
-    #def basicQuery(self):   
     def clean(self):
         for key in self.result_table:
-            print 'trying to close:',key
+            #print 'trying to close:',key
             self.qryClose(key)
+        self.result_table.clear()
     def qryGetResults(self,resultID,startPos = 0,numRows = 100):
         params = '{"resultID" : {"value" : %s}}' % (resultID)
         res = json.loads(self.q('qryGetResults?startPos=%d&numRows=%d&reverse=false' % (startPos,numRows),params))
@@ -141,3 +161,47 @@ class API():
         if res.status != 200 :
             raise Exception(res.reason,res.status)
         return res.read().strip()
+    #https://155.45.179.57/rs/esm/grpGetDeviceTreeEx?displayID=0&hideDisabledDevices=false    
+    def get_device_tree_extended(self):
+        try :
+            response = self.q('grpGetDeviceTreeEx?displayID=0&hideDisabledDevices=true')
+        except Exception as er:				
+            error_info = 'Error getting list of devices:\n %s'%er
+            return (1, error_info)
+        #print response
+        a = json.loads(response);
+        return (0, a['return'])    
+    def get_device_tree(self):
+        try :
+            response = self.q('grpGetDeviceTree?displayID=0&hideDisabledDevices=false')
+        except Exception as er:				
+            error_info = 'Error getting list of devices:\n %s'%er
+            return (1, error_info)
+        a = json.loads(response);
+        
+        return (0, a['return'])    
+    def get_device_list(self, type_of_devs=[]):
+        '''
+        Description: This procedure get the list of devices added to the SIEM architecture
+
+        Input:
+                                Optional type = type is a list wich filter the type of devices to get information from.
+
+        Output:
+                                List object with the id of the device (the id corresponds with the IPSID), IPSID is an
+                                interesting value to filter out information when logs are obtained, type and name of the
+                                device
+        '''
+        if not type_of_devs:
+            type_of_devs = ["IPS","POLICY","RECEIVER","THIRD_PARTY","DBM", "DBM_DB", "DBM_AGENT",\
+                "VA", "IPSVIPS", "ESM", "APM","APMVIPS","ELM","ELMREC","LOCALESM",\
+                "RISK","ASSET","RISKMANAGER","RISKAGENT","EPO","EPO_APP","NSM",\
+                "NSM_SENSOR","NSM_INTERFACE","MVM","OTHER","UNKNOWN"]
+        payload = '{"types": %s}' % ('["' + ('","'.join(type_of_devs)) +  '"]')
+        try:
+            response = self.q('devGetDeviceList?filterByRights=false',payload)
+        except Exception as er:				
+            error_info = 'Error getting list of devices:\n %s'%er
+            return (1, error_info)
+        a = json.loads(response);
+        return (0, a['return'])    
--- src/EsmQueryConfig.py
+++ src/EsmQueryConfig.py
@@ -5,29 +5,49 @@ import sys,json
 if __name__ == "__main__":
     print "This is module, don't run it alone"
     sys.exit(1)
+class EsmFieldFilterElement :
+    element_name = ''
+    element_type = 'EsmBasicValue'
+    element_operator = 'IN'
+    def __init__(self,element_name,element_type = 'EsmBasicValue'):
+        self.element_name = element_name;
+        self.element_type = element_type;
+    def value(self,value):
+        self.element_values = value
+        return self
+    def values(self,values):
+        self.element_values = ','.join(values)
+        return self
+    
+    def operator(self,operator):
+        operator_list = ['IN','NOT_IN','DOES_NOT_EQUAL','EQUALS','CONTAINS','DOES_NOT_CONTAIN']
+        if not operator in operator_list:
+            raise Exception('Unknown operator; acceptable one: IN, NOT_IN, EQUALS')
+        self.element_operator = operator
+        return self
+    def render(self):
+        return """{
+            "type" : "EsmFieldFilter",
+            "field": {"name": "%s"},
+            "operator": "%s",
+            "values": [{
+                "type" : "%s",
+                "value" : "%s"
+            }]
+        }""" % (self.element_name,self.element_operator,self.element_type,self.element_values)
 class SimpleQuery:
     params = dict()
     def __init__(self):
         self.params["includeTotal"] = '"true"';
         self.params["limit"] = '0';
+        self.params["filters"] = [];
     def set_limit(self,limit):
         self.params["limit"] = int(limit);
-    def set_basic_filter(self,field,value):
-        self.params["filters"] = '[{"type" : "EsmFieldFilter","field": {"name": "%s"},"operator": "EQUALS","values": [{    "type" : "EsmBasicValue","value" : "%s"}]}]' % (field,value)
-    def set_complex_filter(self,field,value):
-        sys.exit(100)
-        self.params["filters"] = """
-        [{
-            "type" : "EsmFieldFilter",
-            "field": {"name": "%s"},
-            "operator": "IN",
-            "values": [{
-                "type" : "EsmBasicValue",
-                "value" : "<25"
-            }]
-        }]
-        """ % (field)
-
+    def addNewFilter(self,element_name,element_type = 'EsmBasicValue'):
+        a = EsmFieldFilterElement(element_name,element_type)
+        self.params["filters"].append(a)
+        return a
+        
     def set_fields(self,fields):
         
         #: [{"name": "DSIDSigID"},{"name": "Rule.msg"},{"name": "AvgSeverity"}],
@@ -43,10 +63,42 @@ class SimpleQuery:
         self.params["timeRange"] = '"CUSTOM"'
         self.params["customStart"] = '"%s"' % (start)
         self.params["customEnd"] = '"%s"' % (end)
+    def set_defined_time(self,defName):
+        '''
+        Accepted Values:
+            CUSTOM
+            LAST_MINUTE
+            LAST_10_MINUTES
+            LAST_30_MINUTES
+            LAST_HOUR
+            CURRENT_DAY
+            PREVIOUS_DAY
+            LAST_24_HOURS
+            LAST_2_DAYS
+            LAST_3_DAYS
+            CURRENT_WEEK
+            PREVIOUS_WEEK
+            CURRENT_MONTH
+            PREVIOUS_MONTH
+            CURRENT_QUARTER
+            PREVIOUS_QUARTER
+            CURRENT_YEAR
+            PREVIOUS_YEAR
+        '''
+        self.params["timeRange"] = '"%s"' % (defName)
     def render(self):
         out = ""
         for key in self.params:
-            out+='"%s" : %s,' % (key,self.params[key])
+            if key == 'filters' : 
+                
+                out+='"filters" : ['
+                for skey in self.params[key]:
+                    out+="%s," % (skey.render())
+                if len(self.params[key]) > 0:
+                    out = out[:-1]
+                out+='],'
+            else:
+                out+='"%s" : %s,' % (key,self.params[key])
         out = '{"config": {%s}}' % (out[:-1])
         try:
             json.loads(out)
